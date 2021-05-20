45dc1956b56b4fc795b4b59aa88d3979aef384d1
anero <anero79@gmail.com>
2021-05-20 17:34:36 -0300

    Added rubocop linting

--- Gemfile
+++ Gemfile
@@ -4,5 +4,6 @@ gem 'activesupport', '>= 5.0.0.1', '< 7.0'
 
 group :development do
   gem 'rspec', '~> 3.5'
+  gem 'rubocop'
   gem 'jeweler', '~> 2.2', '>= 2.2.1'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,8 @@ require 'application_service'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), 'support', '**', '*.rb')].sort.each { |f| require f }
 
 RSpec.configure do |config|
-  
+
 end
